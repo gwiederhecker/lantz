6af7dda4a546b43758ac178722661f9b1c0d3a0b
gwiederhecker <gsw@Imac-home.local>
2017-05-24 21:28:49 -0300

    Updated to use PyQt5
    Created new function import_pyqt5, but also did some dirty changes to
    ensure proper loading of PyQt5. Overall I think that this whole
    class/functions need to be changed to handle more elegantlydifferent qt
    versions.

--- lantz/utils/qt_loaders.py
+++ lantz/utils/qt_loaders.py
@@ -215,6 +215,42 @@ def import_pyqt4(version=2):
     api = QT_API_PYQTv1 if version == 1 else QT_API_PYQT
     return QtCore, QtGui, QtSvg, api
 
+def import_pyqt5(version=2):
+    """
+    Import PyQt5
+
+    :param version: 1, 2, or None. Which QString/QVariant API to use.
+                    Set to None to use the system default
+
+    ImportErrors raised within this function are non-recoverable
+    """
+    # The new-style string API (version=2) automatically
+    # converts QStrings to Unicode Python strings. Also, automatically unpacks
+    # QVariants to their underlying objects.
+    import sip
+
+    if version is not None:
+        sip.setapi('QString', version)
+        sip.setapi('QVariant', version)
+
+    from PyQt5 import QtGui, QtCore, QtSvg
+
+    if not check_version(QtCore.PYQT_VERSION_STR, '4.7'):
+        raise ImportError("Lantz requires PyQt4 >= 4.7, found %s" %
+                          QtCore.PYQT_VERSION_STR)
+
+    # Alias PyQt-specific functions for PySide compatibility.
+    QtCore.Signal = QtCore.pyqtSignal
+    QtCore.Slot = QtCore.pyqtSlot
+
+    from PyQt5.uic import loadUi
+    QtGui.loadUi = loadUi
+
+    # query for the API version (in case version == None)
+    version = sip.getapi('QString')
+    api = QT_API_PYQTv1 if version == 1 else QT_API_PYQT
+    return QtCore, QtGui, QtSvg, api  
+                                                    
 
 def import_pyside():
     """
